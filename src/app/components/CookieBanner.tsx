b2876eef275acbd2d3d6db5912a38f2d50b0bd74
Toni Camacho Puigmartí <tonicamacho@MacBook-Pro-de-Toni.local>
2026-05-12 23:57:25 +0200

    feat: add Google Analytics 4 tracking script to CookieBanner

--- src/app/components/CookieBanner.tsx
+++ src/app/components/CookieBanner.tsx
@@ -34,17 +34,31 @@ export default function CookieBanner() {
 
   return (
     <>
-      {/* Load Google Tag Manager only if consent is given */}
+      {/* Load Analytics and Tag Manager only if consent is given */}
       {consent === true && (
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${GTM_ID}');
-          `}
-        </Script>
+        <>
+          {/* Google Analytics 4 */}
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-XC6ZB8SJK0" strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XC6ZB8SJK0');
+            `}
+          </Script>
+
+          {/* Google Tag Manager */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+          </Script>
+        </>
       )}
 
       {/* Show the banner if no decision has been made */}
